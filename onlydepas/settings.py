82dd02b875356aba82682000cd8438242f4a9125
Catalina <cmillan.coronado@gmail.com>
2024-05-17 12:25:34 -0400

    Render2

--- onlydepas/settings.py
+++ onlydepas/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*', 'inmobiliaria-proyectoweb.onrender.com']
-CSRF_TRUSTED_ORIGINS = ['*', 'https://inmobiliaria-proyectoweb.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['*', 'inmobiliaria-proyectoweb.onrender.com']
 
 LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'  # URL a la que se redirige si un usuario no está autenticado y trata de acceder a una vista protegida
