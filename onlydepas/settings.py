07b0fa3a2d19c561e79b556e01c99b1fd263f026
Catalina <cmillan.coronado@gmail.com>
2024-05-17 12:26:29 -0400

    Render3

--- onlydepas/settings.py
+++ onlydepas/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'inmobiliaria-proyectoweb.onrender.com']
+ALLOWED_HOSTS = ['inmobiliaria-proyectoweb.onrender.com']
 CSRF_TRUSTED_ORIGINS = ['*', 'inmobiliaria-proyectoweb.onrender.com']
 
 LOGIN_REDIRECT_URL = 'index'
